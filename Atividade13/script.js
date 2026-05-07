5f06cc97cfc219822dced7d7bf4ec01df6b43c8f
mribeiro25 <163057193+mribeiro25@users.noreply.github.com>
2026-05-06 21:03:41 -0300

    Add files via upload

---
+++ Atividade13/script.js
@@ -0,0 +1,19 @@
+function carregarPagina() {
+    document.getElementById("titulo").innerHTML = "Janela Fechada";
+    document.getElementById("janela").src = "janela-fechada.jpg";
+}
+
+function abrirJanela() {
+    document.getElementById("titulo").innerHTML = "Janela Aberta";
+    document.getElementById("janela").src = "janela-aberta.jpg";
+}
+
+function fecharJanela() {
+    document.getElementById("titulo").innerHTML = "Janela Fechada";
+    document.getElementById("janela").src = "janela-fechada.jpg";
+}
+
+function quebrarJanela() {
+    document.getElementById("titulo").innerHTML = "Janela Quebrada";
+    document.getElementById("janela").src = "janela-quebrada.jpg";
+}
